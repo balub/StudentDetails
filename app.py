01ad5c393273f61de4c8966311bf28d4325a574e
Balu B <balub997@gmail.com>
2019-10-02 18:14:18 +0530

    made the code to fetch all students a bit better

--- app.py
+++ app.py
@@ -14,54 +14,20 @@ api = Api(app)
 
 class AllStudents(Resource):
     def get(self):
-         students = list(collection.find())
-         print(students)
-         return [
-             {
-                "name": students[0]['name'],
-                "phNum": students[0]['phNum'],
-                "email": students[0]['email'],
-                "edu": students[0]['edu'],
-                "college": students[0]['college'],
-                "city": students[0]['city'],
-                "state": students[0]['state'],
-                "country": students[0]['country']
-            },
-             {
-                 "name": students[1]['name'],
-                 "phNum": students[1]['phNum'],
-                 "email": students[1]['email'],
-                 "edu": students[1]['edu'],
-                 "college": students[1]['college'],
-                 "city": students[1]['city'],
-                 "state": students[1]['state'],
-                 "country": students[1]['country']
-             },
-             {
-                 "name": students[2]['name'],
-                 "phNum": students[2]['phNum'],
-                 "email": students[2]['email'],
-                 "edu": students[2]['edu'],
-                 "college": students[2]['college'],
-                 "city": students[2]['city'],
-                 "state": students[2]['state'],
-                 "country": students[2]['country']
-             }
-         ]
-         # for students in collection.find():
-        #     all_students = []
-        #     all_students.append({
-        #         "name": students['name'],
-        #         "phNum": students['phNum'],
-        #         "email": students['email'],
-        #         "edu": students['edu'],
-        #         "college": students['college'],
-        #         "city": students['city'],
-        #         "state": students['state'],
-        #         "country": students['country'],
-        #         "size": collection.count()
-        #     })
-        #     return all_students
+        students = list(collection.find())
+        all_students = []
+        for student in students:
+            all_students.append({
+                "name": student['name'],
+                "phNum": student['phNum'],
+                "email": student['email'],
+                "edu": student['edu'],
+                "college": student['college'],
+                "city": student['city'],
+                "state": student['state'],
+                "country": student['country']
+            })
+        return all_students
 
 
 class Student(Resource):
